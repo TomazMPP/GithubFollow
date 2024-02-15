b1e9ccd54d8fa0c0cc8f8be58059f50cb79a46c3
Tomaz Pontes <tomazmppontes@gmail.com>
2024-02-15 16:19:07 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,7 @@
       <div class="seguidores">
       <h2>PEOPLE THAT DON'T FOLLOW YOU BACK:</h2>
       <div class="usuarios">
-        <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p><p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p><p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p><p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p><p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p><p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p><p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p><p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
-    <p>AAAAAAAAAAAAAAAAAAAA</p>
+       
         <div v-for="user in NonFollow" :key="user">
   <a :href="'https://github.com/' + user.login" target="_blank">
     <p><img :src="user.avatar_url" alt="avatar" class="avatar"> {{ user.login }}</p>
